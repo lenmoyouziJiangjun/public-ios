62d415ae4f43b92b44867b86dcad5f84c22e7fb4
yunhongl <generalray4239@gmail.com>
2016-09-22 10:32:32 +0800

    添加音乐播放工具类

--- util/XMGAudioTool.m
+++ util/XMGAudioTool.m
@@ -12,21 +12,72 @@
 @implementation XMGAudioTool
 
 static NSMutableDictionary *_soundIDs;
+static NSMutableDictionary *_players;
 
 + (void)initialize
 {
     _soundIDs = [NSMutableDictionary dictionary];
+    _players = [NSMutableDictionary dictionary];
 }
 
-//+ (NSMutableDictionary *)soundIDs
-//{
-//    if (_soundIDs == nil) {
-//        _soundIDs = [NSMutableDictionary dictionary];
-//    }
-//    
-//    return _soundIDs;
-//}
++ (void)playMusicWithMusicName:(NSString *)musicName
+{
+    assert(musicName);
+    
+    // 1.定义播放器
+    AVAudioPlayer *player = nil;
+    
+    // 2.从字典中取player,如果取出出来是空,则对应创建对应的播放器
+    player = _players[musicName];
+    if (player == nil) {
+        // 2.1.获取对应音乐资源
+        NSURL *fileUrl = [[NSBundle mainBundle] URLForResource:musicName withExtension:nil];
+        
+        if (fileUrl == nil) return;
+        
+        // 2.2.创建对应的播放器
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileUrl error:nil];
+        
+        // 2.3.将player存入字典中
+        [_players setObject:player forKey:musicName];
+        
+        // 2.4.准备播放
+        [player prepareToPlay];
+    }
+    
+    // 3.播放音乐
+    [player play];
+}
 
++ (void)pauseMusicWithMusicName:(NSString *)musicName
+{
+    assert(musicName);
+    
+    // 1.取出对应的播放
+    AVAudioPlayer *player = _players[musicName];
+    
+    // 2.判断player是否nil
+    if (player) {
+        [player pause];
+    }
+}
+
++ (void)stopMusicWithMusicName:(NSString *)musicName
+{
+    assert(musicName);
+    
+    // 1.取出对应的播放
+    AVAudioPlayer *player = _players[musicName];
+    
+    // 2.判断player是否nil
+    if (player) {
+        [player stop];
+        [_players removeObjectForKey:musicName];
+        player = nil;
+    }
+}
+
+#pragma mark - 音效的播放
 + (void)playSoundWithSoundname:(NSString *)soundname
 {
     // 1.定义SystemSoundID
@@ -35,9 +86,10 @@ static NSMutableDictionary *_soundIDs;
     // 2.从字典中取出对应soundID,如果取出是nil,表示之前没有存放在字典
     soundID = [_soundIDs[soundname] unsignedIntValue];
     if (soundID == 0) {
-        //获取support file文件的url
         CFURLRef url = (__bridge CFURLRef)[[NSBundle mainBundle] URLForResource:soundname withExtension:nil];
-        //创建id
+        
+        if (url == NULL) return;
+        
         AudioServicesCreateSystemSoundID(url, &soundID);
         
         // 将soundID存入字典
